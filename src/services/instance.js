6f98f7a1363f562d4d7b86b6baf25902e3fd8406
kulisjurai <jura.kulis@yahoo.com>
2023-06-16 14:37:42 +0200

    removed logs

--- src/services/instance.js
+++ src/services/instance.js
@@ -4,10 +4,8 @@ export const axiosInstance = async (method, path, setData, data) => {
   if (typeof method === "string") {
     method = method.toLowerCase();
   }
-  console.log("im in", path);
   await axios[method](path, data)
     .then(function (response) {
-      console.log(path, response);
       setData(response.data);
     })
     .catch(function (error) {
